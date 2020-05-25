63b35f818ed866f20660b4b33ae8134e4726e0e5
Scott Schapker <scott.schapker@cleanslatetg.com>
2020-05-25 18:27:49 -0400

    Displays the menu heaers with template outlets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-sandbox';
+  menus: { name: string }[] = [
+    'File',
+    'Edit',
+    'View',
+    'Window',
+  ].map((name) => ({ name }));
 }
--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ContentChild,
+  TemplateRef,
+} from '@angular/core';
 
 @Component({
   selector: 'sb-menubar',
   templateUrl: './menubar.component.html',
-  styleUrls: ['./menubar.component.scss']
+  styleUrls: ['./menubar.component.scss'],
 })
 export class MenubarComponent implements OnInit {
+  @Input() menuItems: { name: string }[];
+  @ContentChild('menuHeaderTemplate') menuHeaderTemplate: TemplateRef<any>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
